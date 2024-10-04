2816a38e07c1661099a57d0ae6fce39821f9a7fc
me_madness <p_rangel@abv.bg>
2024-10-04 14:15:17 +0200

    "feat: adding a few html files"

--- forumApp/posts/forms.py
+++ forumApp/posts/forms.py
@@ -1,5 +1,65 @@
 from django import forms
 
-class PersonForm(forms.Form):
-    person_name = forms.CharField(max_length=10, )
-    age = forms.IntegerField()
+from forumApp.posts.choices import LanguageChoice
+from forumApp.posts.models import Post
+from django.core.exceptions import ValidationError
+
+
+
+# class PersonForm(forms.Form):
+#     person_name = forms.CharField(max_length=10, )
+#     age = forms.IntegerField()
+class PostForm(forms.Form):
+    title = forms.CharField(
+        max_length=100,
+    )
+
+    content = forms.CharField(
+        widget=forms.Textarea,
+    )
+
+    author = forms.CharField(
+        max_length=30,
+    )
+
+    created_at = forms.DateTimeField()
+
+    languages = forms.ChoiceField(
+        choices=LanguageChoice.choices
+    )
+
+
+
+
+class PostBaseForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = "__all__"
+
+class PostCreateForm(PostBaseForm):
+    pass
+
+
+class PostEditForm(PostBaseForm):
+    pass
+
+
+class PostDeleteForm(PostBaseForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields:
+            self.fields[field].disabled = True
+
+
+class SearchForm(forms.Form):
+    query = forms.CharField(
+        label='',
+        required=False,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Search for a post...',
+            }
+        )
+    )
--- forumApp/posts/models.py
+++ forumApp/posts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from forumApp.posts.choices import LanguageChoice
+from forumApp.posts.validators import BadLanguageValidator
+
+
+class Post(models.Model):
+    title = models.CharField(
+        max_length=100,
+    )
+
+    content = models.TextField(
+        validators=(
+            BadLanguageValidator(),
+        )
+    )
+
+    author = models.CharField(
+        max_length=30
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    languages = models.CharField(
+        max_length=20,
+        choices=LanguageChoice.choices,
+        default=LanguageChoice.OTHER,
+    )
