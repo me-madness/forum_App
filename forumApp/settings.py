64d60d6028636d0c433fb9aec29704b067d7cb11
me_madness <p_rangel@abv.bg>
2024-10-31 18:39:09 +0100

    "fix: forumApp app"

--- forumApp/settings.py
+++ forumApp/settings.py
@@ -122,6 +122,9 @@ USE_I18N = True
 USE_TZ = True
 
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
@@ -130,6 +133,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
