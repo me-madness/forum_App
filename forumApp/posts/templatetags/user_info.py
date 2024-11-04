3fab6bcb6ae7df7c3c931c4f6ed90fdb24374d1e
me_madness <p_rangel@abv.bg>
2024-11-04 21:38:21 +0100

    "feat: customUser"

--- forumApp/posts/templatetags/user_info.py
+++ forumApp/posts/templatetags/user_info.py
@@ -2,6 +2,7 @@ from django import template
 
 register = template.Library()
 
+
 @register.inclusion_tag('common/user_info.html')  # takes_context=True
 def user_info(user):
     if user.is_authenticated:
